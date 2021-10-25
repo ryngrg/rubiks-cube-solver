bab1b72fdd0d2e4d615951b1cd36906ad0d13543
ryngrg <garg139@purdue.edu>
2021-10-25 00:03:46 -0400

    updates

--- solver_agent.py
+++ solver_agent.py
@@ -1,42 +1,59 @@
 from cube import Cube
 import random
-import keras
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
 import numpy as np
+
+colors = ('r', 'b', 'w', 'g', 'y', 'o')
 
 
 def random_action():
-    face = random.choice(['r', 'b', 'w', 'g', 'o', 'y'])
+    face = random.choice(colors)
     clockwise = random.choice([True, False])
     return face, clockwise
 
 
+def state_to_input(state):
+    state_list = np.array([colors.index(i) for face in state.values() for i in face])
+    print(state_list)
+    return tf.keras.utils.to_categorical(state_list, num_classes=6)
+
+
 class Network:
     def __init__(self):
-        self.model = keras.model.Sequential()
-        self.model.add(keras.layers.Dense())
+        self.model = Sequential()
+        self.model.add(Dense(36, activation='relu', input_shape=(1, 48, 6)))
+        self.model.add(Dropout(0.2))
+        self.model.add(Dense(24, activation='relu'))
+        self.model.add(Dense(24, activation='relu'))
+        self.model.add(Dropout(0.2))
+        self.model.add(Dense(16, activation='relu'))
+        self.model.add(Dense(12))
 
-    def state_to_input(self, state):
-        pass
+        opt = tf.keras.optimizers.Adam(learning_rate=0.01)
+        self.model.compile(loss="mse", optimizer=opt)
+        self.model.summary()
 
-    def get_q(self, state):
-        inputs = self.state_to_input(state)
-        return self.model(inputs)
+    def get_q(self, state, train=True):
+        inputs = state_to_input(state)
+        return self.model(inputs, training=train)
 
 
 class Agent:
     q_network = Network()
-    
+
     def __init__(self, epsilon=0.01, train=True):
         self.train = train
         self.epsilon = epsilon
         self.actions = []
-        for face in ['r', 'b', 'w', 'g', 'o', 'y']:
+        for face in colors:
             for clockwise in [True, False]:
                 self.actions.append((face, clockwise))
         self.actions = tuple(self.actions)
 
     def greedy_action(self, state):
-        q = self.q_network.get_q(state)
+        q = self.q_network.get_q(state, self.train)
         return self.actions[np.argmax(q)]
 
     def pick_action(self, state):
--- train.py
+++ train.py
@@ -1,15 +1,20 @@
 from cube import Cube
-from solver_agent import Agent
-from solver_agent import random_action
+from solver_agent import Agent, random_action
+from solver_agent import state_to_input
 
 
 def get_shuffled_cube(num_moves):
     shuffled_cube = Cube()
     for i in range(num_moves):
-        shuffled_cube.turn_face(random_action()[0], random_action()[1])
+        face, clockwise = random_action()
+        shuffled_cube.turn_face(face, clockwise)
     return shuffled_cube
 
 
 if __name__ == "__main__":
     rc = get_shuffled_cube(10)
     rc.display()
+    agent = Agent()
+    inputs = state_to_input(rc.get_state())
+    print(inputs.shape)
+    print(inputs)
